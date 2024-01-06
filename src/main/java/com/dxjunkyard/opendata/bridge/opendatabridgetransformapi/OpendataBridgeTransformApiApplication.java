c68fe0a65acb3e155e1467492af0fd7b831d603c
FooQoo <horseshoes.buzz.17@gmail.com>
2024-01-06 10:55:37 +0900

    feat: starter作成

---
+++ src/main/java/com/dxjunkyard/opendata/bridge/opendatabridgetransformapi/OpendataBridgeTransformApiApplication.java
@@ -0,0 +1,13 @@
+package com.dxjunkyard.opendata.bridge.opendatabridgetransformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpendataBridgeTransformApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpendataBridgeTransformApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dxjunkyard/opendata/bridge/opendatabridgetransformapi/OpendataBridgeTransformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxjunkyard.opendata.bridge.opendatabridgetransformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpendataBridgeTransformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
